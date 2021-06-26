1d30d651cac9e5efcb42b963db51ac04dd56d791
Near99 <wkx0823@gmail.com>
2021-06-26 19:26:54 +0800

    v2.0 read status is not ready yet

--- script.js
+++ script.js
@@ -1,187 +1,139 @@
-const wrapper = document.querySelector('.bslp');
-const closeFormContainer = document.querySelector('.close-form-container');
-const formContainer = document.querySelector('.form-container');
-const bookInfoForm = document.querySelector('.book-info');
-const bookInfo = ['title', 'author', 'pages', 'language', 'year'];
-const propertyStr = ['', 'By', 'Number of Pages:', 'Language:', 'Published:'];
-let myLibrary = []; // Store this array somewhere
+const formSubmit = document.querySelector('.submit-form');
+const addBookButton = document.querySelector('.add-book-button');
+const formContainer = document.querySelector('.container');
+const closeFormButton = document.querySelector('.form-close-button');
+const grid = document.querySelector('.grid');
+let removeBookButton = undefined;
+let bookList = [];
 
-function Book(title, author, pages, language, year, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.language = language;
-  this.year = year;
-  this.read = read;
-}
-
-function addBookToLibrary(title, author, pages, language, year, read) {
-  myLibrary.push(new Book(title, author, pages, language, year, read));
-  reset();
-  init();
-}
-
-myLibrary.push(new Book('JS', 'Charles', '399', 'English', '1996', false));
-myLibrary.push(new Book('Node.js', 'Charles', '399', 'English', '1996', true));
-
-function createBookCards() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    const newCard = document.createElement('div');
-    const newUL = document.createElement('ul');
-    newCard.setAttribute('data-book', `${i}`);
-    wrapper.appendChild(newCard);
-    newUL.setAttribute('data-ul', `${i}`);
-    if (myLibrary[i].read === true) {
-      newUL.classList.add('haveRead');
-    }
-    newCard.appendChild(newUL);
-    for (let j = 0; j < bookInfo.length; j++) {
-      const newLI = document.createElement('li');
-      newLI.setAttribute('data-li', `${j}`);
-      newUL.appendChild(newLI);
-    }
+class Book {
+  constructor(title, author, page, year, language, read) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.year = year;
+    this.language = language;
+    this.read = read;
   }
 }
 
-function displayBooks() {
-  const allUL = Array.from(document.querySelectorAll(`[data-ul]`));
-  const bookTitle = Array.from(document.querySelectorAll(`[data-li='${0}']`));
-  for (let i = 0; i < allUL.length; i++) {
-    for (let j = 0; j < bookInfo.length; j++) {
-      // For debug use
-      // console.log(allUL[i].childNodes[j]);
-      allUL[i].childNodes[j].innerHTML = `${propertyStr[j]} ${
-        myLibrary[i][bookInfo[j]]
-      }`;
-    }
-  }
-  // Make book title bold.
-  bookTitle.forEach((item) => item.classList.add('book-titles'));
-}
-
-function addNewBookButton() {
-  const buttonDiv = document.createElement('div');
-  const button = document.createElement('button');
-  button.classList.add('addNewBook');
-  buttonDiv.setAttribute('data-addBook', '0');
-  button.innerHTML = '+';
-  wrapper.appendChild(buttonDiv);
-  buttonDiv.appendChild(button);
-  button.addEventListener('click', () => {
-    // addBookToLibrary();
-    formContainer.classList.add('active');
-    closeFormContainer.addEventListener('click', () => {
-      formContainer.classList.remove('active');
+class Display {
+  static displayBook() {
+    bookList.forEach((book) => {
+      this.addBookToList(book);
     });
-  });
+  }
+
+  static addBookToList(book) {
+    const newCard = document.createElement('div');
+    let readingStatus = 'Reading';
+    newCard.classList.add('card');
+    // If the book is been read, add have-read class and set readingStatus to Done Reading;
+    if (book.read === true) {
+      newCard.classList.add('have-read');
+      readingStatus = 'Done Reading';
+    }
+    newCard.innerHTML = `
+      <div class="remove">
+       <p>×</p>
+      </div>
+      <div class="header">
+       <h2>${book.title}</h2>
+      </div>
+      <div class="book-info">
+       <h4>Written by ${book.author}</h4>
+       <h4>${book.page} Pages</h4>
+       <h4>Published in ${book.year}</h4>
+       <h4>Language: ${book.language}</h4>
+      </div>
+      <div class="read-status">
+       <p class="read-status-text">${readingStatus}</p>
+      </div>
+    `;
+    grid.appendChild(newCard);
+    // Select all elements with remove class when new book is added;
+    removeBookButton = Array.from(document.querySelectorAll('.remove'));
+    // Call remove function to update eventListener status;
+    Display.removeBookFromList();
+  }
+
+  static removeBookFromList() {
+    removeBookButton.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        // console.log(e.target.parentElement.parentElement.children[1].innerText);
+        // Remove the book from booklist array;
+        // Here I assume no two books share the same name :>;
+        // It should have a unique identifier for this job, but anyways..
+        bookList.forEach((book, index) => {
+          if (
+            book.title ===
+            e.target.parentElement.parentElement.children[1].innerText
+          ) {
+            bookList.splice(index, 1);
+          }
+        });
+        // Remove DOM element;
+        e.target.parentElement.parentElement.remove();
+      });
+    });
+  }
 }
 
-function addNewBookForm() {
-  bookInfoForm.addEventListener('submit', (e) => {
+// Push book information object to bookList arr;
+const addNewBook = (title, author, page, year, language, read) => {
+  bookList.push(new Book(title, author, page, year, language, read));
+};
+
+// Add new book form events control;
+const formEventController = () => {
+  addBookButton.addEventListener('click', () => {
+    formContainer.classList.add('active');
+  });
+
+  formSubmit.addEventListener('submit', (e) => {
     const titleInput = document.getElementById('title').value;
     const authorInput = document.getElementById('author').value;
     const pageInput = document.getElementById('page').value;
-    const languageInput = document.getElementById('language').value;
     const yearInput = document.getElementById('year').value;
-    const readInput = document.getElementById('read').checked;
-    e.preventDefault();
-    addBookToLibrary(
+    const languageInput = document.getElementById('language').value;
+    let tmpBookInfo = new Book(
       titleInput,
       authorInput,
       pageInput,
-      languageInput,
       yearInput,
-      readInput
+      languageInput
     );
+    e.preventDefault();
+    addNewBook(titleInput, authorInput, pageInput, yearInput, languageInput);
     formContainer.classList.remove('active');
-    bookInfoForm.reset();
+    formSubmit.reset();
+    Display.addBookToList(tmpBookInfo);
   });
-}
 
-function removeAndReadButton() {
-  const allBook = Array.from(document.querySelectorAll(`[data-book]`));
-  for (let i = 0; i < allBook.length; i++) {
-    const buttonContainer = document.createElement('div');
-    const removeButton = document.createElement('button');
-    const readButton = document.createElement('button');
-    buttonContainer.classList.add('rmarbut');
-    removeButton.setAttribute('data-remove', `${i}`);
-    readButton.setAttribute('data-read', `${i}`);
-    removeButton.innerHTML = 'Remove';
-    readButton.innerHTML = 'Read';
-    allBook[i].appendChild(buttonContainer);
-    buttonContainer.appendChild(readButton);
-    buttonContainer.appendChild(removeButton);
-  }
-  const removeBook = document.querySelectorAll('[data-remove]');
-  removeBook.forEach((r) =>
-    r.addEventListener('click', (e) => {
-      for (let j = 0; j < myLibrary.length; j++) {
-        if (Number(e.target.attributes[0].nodeValue) === j) {
-          // For debug use
-          // console.log(e.target.attributes[0].nodeValue);
-          myLibrary.splice(j, 1);
-          reset();
-          init();
-        }
-      }
-    })
-  );
-}
+  closeFormButton.addEventListener('click', () => {
+    formContainer.classList.remove('active');
+  });
+};
 
-function updateReadStatus() {
-  const allBook = Array.from(document.querySelectorAll(`[data-read]`));
-  allBook.forEach((ul) =>
-    ul.addEventListener('click', (e) => {
-      // For Debug use
-      // console.log(e.target.parentElement.parentElement.firstChild);
-      // console.log(e.target.parentNode.parentNode.attributes[0].nodeValue);
-      if (
-        myLibrary[
-          Number(e.target.parentNode.parentNode.attributes[0].nodeValue)
-        ].read == true
-      ) {
-        e.target.parentElement.parentElement.firstChild.classList.remove(
-          'haveRead'
-        );
-        myLibrary[
-          Number(e.target.parentNode.parentNode.attributes[0].nodeValue)
-        ].read = false;
-        return;
-      }
+addNewBook(
+  'The Old Man and The Sea',
+  'Charles Wong',
+  222,
+  '1959',
+  'English',
+  false
+);
+addNewBook(
+  'Introduction to Algorithms',
+  'Charles Wong',
+  235,
+  '2001',
+  'English',
+  false
+);
 
-      if (
-        myLibrary[
-          Number(e.target.parentNode.parentNode.attributes[0].nodeValue)
-        ].read !== true
-      ) {
-        myLibrary[
-          Number(e.target.parentNode.parentNode.attributes[0].nodeValue)
-        ].read = true;
-        e.target.parentElement.parentElement.firstChild.classList.add(
-          'haveRead'
-        );
-      }
-    })
-  );
-}
-
-function reset() {
-  const allBook = Array.from(document.querySelectorAll(`[data-book]`));
-  const removeBookButton = document.querySelector('[data-addbook]');
-  removeBookButton.remove();
-  for (let i = 0; i < allBook.length; i++) {
-    allBook[i].remove();
-  }
-}
-
-function init() {
-  createBookCards();
-  displayBooks();
-  addNewBookButton();
-  addNewBookForm();
-  removeAndReadButton();
-  updateReadStatus();
-}
-
-init();
+// Display books and active form event controller;
+document.addEventListener('DOMContentLoaded', () => {
+  Display.displayBook();
+  formEventController();
+});
